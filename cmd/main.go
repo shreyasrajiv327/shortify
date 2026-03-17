3e7fdae27c33e20ca009b8d6651510ab01392178
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2026-03-17 16:42:04 +0530

    setup main server with routing for URL shortener

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
-package cmd
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"shortify/internal/handlers"
+	"shortify/internal/repository"
+	"shortify/internal/services"
+)
+
+func main(){
+
+	repository := repository.NewURLRepository()
+	service := services.NewURLService(repository)
+	handler := handlers.NewURLHandelr(service)
+	http.HandleFunc("/shorten", handler.CreateShortURL)
+	http.HandleFunc("/", handler.RedirectURL)
+
+	log.Println("Server running on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
