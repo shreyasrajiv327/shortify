2dea0fe768cf1f7fa0cce3f21daf89263f8d6aa5
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2026-03-25 15:48:59 +0530

    feat: add input validation for URL shortening endpoint

--- internal/handlers/url_handler.go
+++ internal/handlers/url_handler.go
@@ -3,13 +3,18 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"net/url"
 	"strings"
-
+     "shortify/internal/logger"
 	"shortify/internal/services"
 )
 
 type URLHandler struct {
 	service *services.URLService
+}
+
+type CreateURLRequest struct {
+	URL string `json:"url"`
 }
 
 func NewURLHandler(service *services.URLService) *URLHandler {
@@ -18,12 +23,21 @@ func NewURLHandler(service *services.URLService) *URLHandler {
 	}
 }
 
-// Request struct (clean + reusable)
-type CreateURLRequest struct {
-	URL string `json:"url"`
+
+func isValidURL(input string) bool {
+	parsed, err := url.ParseRequestURI(input)
+	if err!= nil{
+		return false
+	}
+
+	if parsed.Scheme !="http" && parsed.Scheme != "https"{
+		return false
+	}
+
+	return true
 }
 
-// POST /shorten
+
 func (h *URLHandler) CreateShortURL(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -36,17 +50,32 @@ func (h *URLHandler) CreateShortURL(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
+		logger.Log.Warn("Invalid request body")
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	if request.URL == "" {
+		logger.Log.Warn("Empty URL recieved")
 		http.Error(w, "URL is required", http.StatusBadRequest)
+		return
+	}
+
+	if len(request.URL)>2048{
+		logger.Log.Warn("URL too long","url",request.URL)
+		http.Error(w, "URL too long", http.StatusBadRequest)
+		return
+	}
+
+	if !isValidURL(request.URL){
+		logger.Log.Warn("Invalid URL format", "url", request.URL)
+		http.Error(w, "Invalid URL format", http.StatusBadRequest)
 		return
 	}
 
 	url, err := h.service.CreateShortURL(request.URL)
 	if err != nil {
+		logger.Log.Error("Failed to create short URL", "error", err)
 		http.Error(w, "Failed to create short URL", http.StatusInternalServerError)
 		return
 	}
@@ -60,7 +89,7 @@ func (h *URLHandler) CreateShortURL(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-// GET /{shortCode}
+
 func (h *URLHandler) RedirectURL(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
